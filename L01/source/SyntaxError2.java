58f2ab7e73f8d79ec46107009779bc8e38c5c6a2
bosswt <boss.wt13@gmail.com>
2020-01-06 11:03:04 +0700

    L01

---
+++ L01/source/SyntaxError2.java
@@ -0,0 +1,7 @@
+public class SyntaxError2
+{
+	public static void main(String [] args) 
+	{
+		System.out.println(Hello World!);
+	}
+}
---
+++ L01/source/SyntaxError3.java
@@ -0,0 +1,7 @@
+public class SyntaxError
+{
+	public static void main(String [] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
