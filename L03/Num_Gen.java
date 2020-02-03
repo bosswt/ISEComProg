a9a7250340d2b4b2d5cccdfb6ee6c4b2311cda98
bosswt <boss.wt13@gmail.com>
2020-02-03 11:15:38 +0700

    Use Math.max() method

--- L03/Num_Gen.java
+++ L03/Num_Gen.java
@@ -6,13 +6,7 @@ public class Num_Gen {
         Random rn = new Random();
         for (int i = 0; i < 30; i++) {
             double randomNum = Math.ceil(rn.nextGaussian() * 2 + 5);
-            if (randomNum < 1) {
-                randomNum = 1;
-            }
-            if (randomNum > 10) {
-                randomNum = 10;
-            }
-            System.out.print(randomNum + ",");
+            System.out.print(Math.max(1, Math.min(randomNum, 10)) + ",");
         }
     }
 }
